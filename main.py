ecd5a2679ee36f29feb1f46590e094bfb2f90081
Maxim Yakovlev <it.trainee@yandex.ru>
2022-11-20 22:40:17 +0300

    refactoring

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ def recur_factorial(n):
         temp = temp * n
         return temp
     print(recur_factorial(5))
+
+
+def recur_factorial(n):
+    if n == 1:
+        return n
+    else:
+        return n * recur_factorial(n - 1)
+    print(recur_factorial(5))
