4c43d5a59c5d5690c711c20a79ecc0a5febc03dd
Maxim Yakovlev <it.trainee@yandex.ru>
2022-11-21 23:53:46 +0300

    the permutation algorithm

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def iterative_factorial(n):
     for i in range(2, n + 1):
         fact *= i
         return fact
-    print(iterative_factorial(5))
+
+print(iterative_factorial(5))
 
 
 def recur_factorial(n):
@@ -13,7 +14,8 @@ def recur_factorial(n):
         temp = recur_factorial(n - 1)
         temp = temp * n
         return temp
-    print(recur_factorial(5))
+
+print(recur_factorial(5))
 
 
 def recur_factorial(n):
@@ -21,4 +23,19 @@ def recur_factorial(n):
         return n
     else:
         return n * recur_factorial(n - 1)
-    print(recur_factorial(5))
+
+print(recur_factorial(5))
+
+
+def permute(string, pocket=""):
+    if len(string) == 0:
+        print(pocket)
+    else:
+        for i in range(len(string)):
+            letter = string[i]
+            front = string[0:i]
+            back = string[i+1:]
+            together = front + back
+            permute(together, letter + pocket)
+
+print(permute("ABC", ""))
